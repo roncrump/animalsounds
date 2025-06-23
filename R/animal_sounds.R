9aa067179dffeeeb4cf574d35ba9ac3a3d2273ec
Ron Crump <r.e.crump@warwick.ac.uk>
2025-06-23 13:26:40 +0100

    Added some documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,13 @@
+#' Tell us what sounds an animal makes
+#'
+#' @param animal the name of an animal (a single character string)
+#' @param sound the sound made by an animal (a single character string)
+#'
+#' @returns A string like this: "The (animal) goes (sound)"
+#' @export
+#'
+#' @examples
+#' animal_sounds("peacock","BlEuRk!")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
